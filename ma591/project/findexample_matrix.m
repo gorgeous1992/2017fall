c750d97ccbe1e8d5dab986f434d15bf34fe0a20a
gorgeous1992 <tonygaobasketball@hotmail.com>
2017-11-19 15:11:50 -0500

    edit matlab files

--- ma591/project/findexample_matrix.m
+++ ma591/project/findexample_matrix.m
@@ -7,12 +7,14 @@ function IDX = findexample_matrix(A, X)
 
 % # of inequalities.
 n = length(A(1,1,:));
-epsi = 1e-04;
+epsi = 1e-03;
 
-for i = 1 : n
+for i = 1 : n 
+     trace(A(:,:,i)'*X)
     if trace(A(:,:,i)'*X) < -epsi
+       
 %    if trace(A(:,:,i)'*X) < 0
-        IDX = i;
+        IDX = i
         return
     end
 end
@@ -21,4 +23,22 @@ IDX = -1
 pause
 return 
 
+% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% %|A_j X | < \epsilon   version
+% for i = 1 : n
+%     if abs(trace(A(:,:,i)'*X)) < epsi
+%         continue
+%     else
+%         IDX = i;
+%         return
+%     end
+% end
+% 
+% IDX = -1
+% pause
+% return
+% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+end
+        
 
---
+++ ma591/project/findexample_maxcut.m
@@ -0,0 +1,23 @@
+%This function is to find index (The smallest) of A_j such that
+% A_j X < 0;
+%Input: 3-dimensional tensor A and distribution X;
+%Output: the smallest index j such that A_j X <0;
+
+function IDX = findexample_matrix(A, X)
+
+% # of inequalities.
+n = length(A(1,1,:));
+epsi = 1e-06;
+
+for i = 1 : n 
+     trace(A(:,:,i)'*X)
+    if abs(trace(A(:,:,i)'*X)) > epsi
+       continue
+    else IDX = i
+        return
+    end
+end
+
+IDX = -1
+pause
+return 
